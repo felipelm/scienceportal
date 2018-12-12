b9ebb20b5dc3f1370f9b2e75a86c8bfa51fffd15
felipe <felipelm123@gmail.com>
2018-12-12 16:15:53 -0200

    updating build

--- configure.sh
+++ configure.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 git submodule init
 git submodule update
 cp sirius/.env.template sirius/.env
 cp orion/.env.template orion/.env
+cp deployment/nginx-proxy.conf.template deployment/nginx-proxy.conf
 cp docker-compose.yml.template docker-compose.yml
+echo 'Building'
+docker-compose build
+echo 'Starting'
+docker-compose up
