e96ee4f0c97d2341baaf2ea169a407f96afb3a81
felipe <felipelm123@gmail.com>
2018-12-12 16:21:04 -0200

    removing unused file and updating configure

--- configure.sh
+++ configure.sh
@@ -3,6 +3,7 @@ git submodule init
 git submodule update
 cp sirius/.env.template sirius/.env
 cp orion/.env.template orion/.env
+cp centaurus/global-env.template centaurus/global-env
 cp deployment/nginx-proxy.conf.template deployment/nginx-proxy.conf
 cp docker-compose.yml.template docker-compose.yml
 echo 'Building'
